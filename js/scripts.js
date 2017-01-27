18319c182e05641cc794c7676c9e55c133426107
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-27 12:28:34 -0800

    add price array

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,6 @@ function Pizza(size, toppings, price){
   alert("price: " + this.pizzaPrice);
 }
 
-
-
 $(document).ready(function(){
   $("#readyToOrderButton").click(function(){
     $("#readyToOrderSection").toggle();
@@ -17,30 +15,24 @@ $(document).ready(function(){
     $("#orderPizzaButton").click(function(event){
       event.preventDefault();
       var size= $("#pizzaDetailsForm input[type='radio']:checked").val();
-
       var toppings = [];
-
+      var price = [];
       $("#pizzaDetailsForm input[type='checkbox']:checked").each(function(){
         toppings.push($(this).val());
-      });
-      var price = [];
-
-      for (var i = 0; i < toppings.length; i++)
-      {
         price.push(2);
-        if (size === 'Small')
-        {
-          price.push(5);
-        }
-        else if (size === "Medium"){
-          price.push(10);
-        }
-        else if (size === "Large") {
-          price.push(20);
-        }
+      });
+      if (size === 'Small')
+      {
+        price.push(5);
+      } else if (size === "Medium"){
+        price.push(10);
+      } else {
+        price.push(20);
       }
 
+
       var newPizza = new Pizza(size, toppings, price);
+      // $("#")
 
     });
   });
