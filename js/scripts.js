4ff3cd536b756aad19ad2aaeac9e59e419808935
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-27 12:11:03 -0800

    add function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
+function Pizza(size, toppings, price){
+
+  this.pizzaSize = size;
+  this.pizzaToppings = [toppings];
+  this.pizzaPrice = [price];
+  alert("size: " + this.pizzaSize);
+  alert("toppings: " + this.pizzaToppings);
+  alert("price: " + this.pizzaPrice);
+}
+
 
 
 $(document).ready(function(){
   $("#readyToOrderButton").click(function(){
     $("#readyToOrderSection").toggle();
-
     $("#showForm").show();
-
     $("#orderPizzaButton").click(function(event){
       event.preventDefault();
       var size= $("#pizzaDetailsForm input[type='radio']:checked").val();
-      alert(size);
-      var toppings = $("#pizzaDetailsForm input[type='check-box']:checked").val();
-      alert(toppings);
+
+      var toppings = [];
+
+      $("#pizzaDetailsForm input[type='checkbox']:checked").each(function(){
+        toppings.push($(this).val());
+      });
+      var price = [];
+
+      for (var i = 0; i < toppings.length; i++)
+      {
+        price.push(2);
+        if (size === 'Small')
+        {
+          price.push(5);
+        }
+        else if (size === "Medium"){
+          price.push(10);
+        }
+        else if (size === "Large") {
+          price.push(20);
+        }
+      }
+
+      var newPizza = new Pizza(size, toppings, price);
+
     });
   });
 });
