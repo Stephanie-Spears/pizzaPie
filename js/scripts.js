ba9ef14427d401ae7d9aa0ba3c8f6d031d1e03d0
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-29 20:16:45 -0800

    remove array and prototypes from JS, simplify

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,14 @@ function Pizza(){
   this.pizzaSize = "";
   this.pizzaToppings = [];
   this.pizzaPrice = 0;
-
 }
 
-function PizzaList(){
-  this.pizzaList = [];
-  this.pizzaListTotal = 0;
-
-}
-
-Pizza.prototype.getPizzaToppings = function (){
+Pizza.prototype.getPizzaPrice = function(total){
   var toppings = [];
   $("#pizzaDetailsForm input[type='checkbox']:checked").each(function(){
     toppings.push($(this).val());
   });
   this.pizzaToppings = toppings;
-  return this.pizzaToppings;
-};
-
-Pizza.prototype.getPizzaPrice = function(){
   this.pizzaSize = $("#pizzaDetailsForm input[type='radio']:checked").val();
   for (var i = 0; i < this.pizzaToppings.length; i++)
   {
@@ -33,44 +22,33 @@ Pizza.prototype.getPizzaPrice = function(){
   } else if (this.pizzaSize === 'Large') {
     this.pizzaPrice += 20;
   }
+
   return this.pizzaPrice;
 };
 
-PizzaList.prototype.calculateTotalPrice = function(){
-  var totalPrice = 0;
-  for (var i=0; i< this.pizzaList.length; i++){
-
-    totalPrice += this.pizzaList[i].pizzaPrice;
-  }
-  this.pizzaListTotal = totalPrice;
-  return this.PizzaListTotal;
-};
-
 Pizza.prototype.formatOutput = function() {
-  return "<button class='list-group-item'>" +  this.pizzaSize + " " + this.pizzaToppings + " pizza " + "($" + this.pizzaPrice.toFixed(2) + ")<span class='glyphicon glyphicon-heart-empty' aria-hidden='true'></span></button>";
+  return "<button type='click' class='list-group-item'>" +  this.pizzaSize + " " + this.pizzaToppings + " pizza " + "($" + this.pizzaPrice.toFixed(2) + ")<span class='glyphicon glyphicon-heart-empty' aria-hidden='true'></span></button>";
 };
 
 
 $(document).ready(function(){
-  var myPizzaList = new PizzaList();
   $("#readyToOrderButton").click(function(){
     $(this).remove();
     $("#showForm").show();
-
+    var total = 0;
     $("#orderPizzaButton").click(function(event){
       event.preventDefault();
-      var myPizza = new Pizza();
+      var myPizza = null;
+      myPizza = new Pizza();
       $("#customerOrder").show();
       $("#checkoutButton").show();
-
-      myPizza.getPizzaToppings();
-      myPizza.getPizzaPrice();
+      $("#removePizzaButton").show();
+      // myPizza.getPizzaToppings();
+      myPizza.getPizzaPrice(total);
       $(".pizzaList").prepend(myPizza.formatOutput());
+      total += myPizza.pizzaPrice;
 
-      myPizzaList.pizzaList.push(myPizza);
-      // alert(Object.values(myPizzaList));
-      myPizzaList.calculateTotalPrice();
-      $("#showTotal").text("$" + myPizzaList.pizzaListTotal.toFixed(2));
+      $("#showTotal").text("$" + total.toFixed(2));
       $("#checkoutButton").click(function(event){
         event.preventDefault();
         $("#showForm").hide();
